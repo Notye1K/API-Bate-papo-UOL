d904c8b14dbb5620bb153a426e8b64e29f8b69fd
Kl3yton <kleyton_vccamargo@hotmail.com>
2022-01-29 16:12:58 -0300

    feat: add delete and put on /message/id

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express, { json } from "express"
 import cors from "cors"
 import dotenv from "dotenv"
-import { MongoClient } from 'mongodb'
+import { MongoClient, ObjectId } from 'mongodb'
 import dayjs from 'dayjs'
 import joi from 'joi'
 import { stripHtml } from "string-strip-html";
@@ -48,11 +48,10 @@ app.post('/participants', async (req, res) => {
 
         res.sendStatus(201)
 
-    } catch (err){
+    } catch (err) {
         res.sendStatus(500)
-        console.log(err);
     } finally {
-        mongoClient.close()
+        await mongoClient.close()
     }
 })
 
@@ -66,7 +65,7 @@ app.get('/participants', async (req, res) => {
     } catch {
         res.sendStatus(500)
     } finally {
-        mongoClient.close()
+        await mongoClient.close()
     }
 })
 
@@ -85,7 +84,7 @@ app.post('/messages', async (req, res) => {
             res.status(422).send(erros)
             return
         }
-        if (req.body.type !== 'message' && req.body.type !== 'private_message'){
+        if (req.body.type !== 'message' && req.body.type !== 'private_message') {
             res.status(422).send('type must be message or private_message')
             return
         }
@@ -95,15 +94,15 @@ app.post('/messages', async (req, res) => {
         const client = await mongoClient.connect()
         const db = client.db(dbName)
         const usersCollection = db.collection(usersCollectionName)
-        const users = await usersCollection.findOne({ user })
+        const users = await usersCollection.findOne({ name: user })
 
-        if(users){
+        if (!users) {
             res.status(422).send('user does not exist or has been logged out')
             return
         }
 
         const messagesCollection = db.collection(messagesCollectionName)
-        let {to, text, type} = req.body
+        let { to, text, type } = req.body
         to = stripHtml(to).result.trim()
         text = stripHtml(text).result.trim()
         type = stripHtml(type).result.trim()
@@ -111,10 +110,10 @@ app.post('/messages', async (req, res) => {
         await messagesCollection.insertOne({ from: user, to, text, type, time })
 
         res.sendStatus(201)
-    } catch {
+    } catch (error) {
         res.sendStatus(500)
-    } finally{
-        mongoClient.close()
+    } finally {
+        await mongoClient.close()
     }
 })
 
@@ -134,7 +133,7 @@ app.get('/messages', async (req, res) => {
     } catch {
         res.sendStatus(500)
     } finally {
-        mongoClient.close()
+        await mongoClient.close()
     }
 })
 
@@ -154,10 +153,109 @@ app.post('/status', async (req, res) => {
 
         await usersCollection.updateOne({ name }, { $set: { lastStatus: Date.now() } })
         res.sendStatus(200)
-    } catch {
+    } catch (error) {
         res.sendStatus(500)
     } finally {
-        mongoClient.close()
+        await mongoClient.close()
+    }
+})
+
+app.delete('/messages/:id', async (req, res) => {
+    try {
+        const user = stripHtml(req.headers.user).result.trim()
+        const id = req.params.id
+        if (id.length !== 24) {
+            res.sendStatus(404)
+            return
+        }
+
+        const client = await mongoClient.connect()
+        const db = client.db(dbName)
+        const messagesCollection = db.collection(messagesCollectionName)
+        const message = await messagesCollection.findOne({ _id: new ObjectId(id) })
+
+        if (!message) {
+            res.sendStatus(404)
+            return
+        }
+
+        if (message.from !== user) {
+            res.sendStatus(401)
+            return
+        }
+
+        await messagesCollection.deleteOne({ _id: new ObjectId(id) })
+        res.sendStatus(200)
+
+    } catch (error) {
+        res.sendStatus(500)
+    } finally {
+        await mongoClient.close()
+    }
+})
+
+app.put('/messages/:id', async (req, res) => {
+    try {
+        const schema = joi.object(
+            {
+                to: joi.string().required(),
+                text: joi.string().required(),
+                type: joi.string().required()
+            })
+        const validation = schema.validate(req.body, { abortEarly: false })
+
+        if (validation.error) {
+            const erros = validation.error.details.map(v => v.message)
+            res.status(422).send(erros)
+            return
+        }
+        if (req.body.type !== 'message' && req.body.type !== 'private_message') {
+            res.status(422).send('type must be message or private_message')
+            return
+        }
+
+        const user = stripHtml(req.headers.user).result.trim()
+
+        const client = await mongoClient.connect()
+        const db = client.db(dbName)
+        const usersCollection = db.collection(usersCollectionName)
+        const users = await usersCollection.findOne({ name: user })
+
+        if (!users) {
+            res.status(422).send('user does not exist or has been logged out')
+            return
+        }
+
+        const id = req.params.id
+        if (id.length !== 24) {
+            res.sendStatus(404)
+            return
+        }
+
+        const messagesCollection = db.collection(messagesCollectionName)
+        const message = await messagesCollection.findOne({ _id: new ObjectId(id) })
+
+        if (!message) {
+            res.sendStatus(404)
+            return
+        }
+        if (message.from !== user) {
+            res.sendStatus(401)
+            return
+        }
+
+        let { to, text, type } = req.body
+        to = stripHtml(to).result.trim()
+        text = stripHtml(text).result.trim()
+        type = stripHtml(type).result.trim()
+        const time = dayjs().format('HH:mm:ss')
+        await messagesCollection.updateOne({ _id: new ObjectId(id) }, { $set: { to, text, type, time } })
+        res.sendStatus(200)
+
+    } catch (error) {
+        res.sendStatus(500)
+    } finally {
+        await mongoClient.close()
     }
 })
 
